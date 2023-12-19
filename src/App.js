7be1233af540bf0e7ed7b234ae3b3f1734459701
Mohd Aquib <akibkhan9984@gmail.com>
2023-12-19 23:45:42 +0530

    Header Components Build

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AppContext } from './context/ContextApi'
+import Header from './components/Header'
+import Feeds from './components/Feeds'
+import SearchResult from './components/SearchResult'
+import VideoDetails from './components/VideoDetails'
 
 const App = () => {
   return (
     <>
-      <AppContext/>
+      <AppContext>
+        <BrowserRouter>
+          <div className='flex flex-col h-full'>
+            <Header />
+            <Routes>
+              <Route path="/" exact element={<Feeds />} />
+              <Route path="/searchResult/:searchQuery" element={<SearchResult />} />
+              <Route path="/video/:id" element={<VideoDetails/>} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AppContext>
+     
     </>
   )
 }
